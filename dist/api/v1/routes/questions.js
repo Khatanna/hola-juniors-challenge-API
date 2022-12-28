d1c177d92ccb0e0dd6b6cd5fd3060d57bc8ba987
Charlie <carlos65357@gmail.com>
2022-12-28 11:59:42 -0400

    prep: enviroment variable

--- dist/api/v1/routes/questions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const questions_controller_1 = require("../controllers/questions.controller");
-const router = (0, express_1.Router)();
-router.get("/", questions_controller_1.getAllQuestions);
-exports.default = router;
